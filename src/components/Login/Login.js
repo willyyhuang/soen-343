c0a74e9572d212464144a067a74e13b41a167595
Wilson Wei Chen Huang <48188884+willyyhuang@users.noreply.github.com>
2020-10-02 14:59:33 -0400

    chore: update login page styling

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,17 +1,26 @@
 import {
-  Button, Card, Col, Input, Row,
+  Button, Card, Col, Divider, Input, Row, Typography,
 } from 'antd'
 import React from 'react'
+import placeholderLogo from '../../images/placeholderLogo.jpg'
 import './index.css'
 
 const Login = () => (
   <Row type='flex' align='middle'>
     <Col span={8} />
-    <Col span={8}>
-      <Card style={{marginTop: '40%'}}>
-        <Input placeholder='username' />
+    <Col span={8} className='login'>
+      <img src={placeholderLogo} className='logo' />
+      <Card style={{backgroundColor: '#FBF8F8'}}>
+        <Input placeholder='username or email' />
         <Input.Password className='row' placeholder='password' />
-        <Button className='row' block type='ghost'>Login</Button>
+        <Button className='row' block style={{backgroundColor: '#5FAEF3', color: 'white'}} type='ghost'>Log In</Button>
+      </Card>
+      <Divider />
+      <Card style={{backgroundColor: '#FBF8F8'}}>
+        <Typography.Text>
+          Don&apos;t have an account?
+        </Typography.Text>
+        <Button type='link'>Sign up here!</Button>
       </Card>
     </Col>
     <Col span={8} />
