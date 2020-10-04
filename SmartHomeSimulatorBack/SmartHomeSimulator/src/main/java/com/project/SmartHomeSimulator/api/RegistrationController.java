8e97b77f9f22389686cb924f706180e6694b2c87
karin kazarian <karinghazarian@gmail.com>
2020-10-04 17:52:01 -0400

    added swagger for api documentation

--- SmartHomeSimulatorBack/SmartHomeSimulator/src/main/java/com/project/SmartHomeSimulator/api/RegistrationController.java
+++ SmartHomeSimulatorBack/SmartHomeSimulator/src/main/java/com/project/SmartHomeSimulator/api/RegistrationController.java
@@ -4,20 +4,18 @@ import com.project.SmartHomeSimulator.model.User;
 import com.project.SmartHomeSimulator.service.RegistrationService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.ResponseStatus;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
 
 @RestController
+@RequestMapping("/api/v1")
 public class RegistrationController {
 
     @Autowired
     private RegistrationService registrationService;
 
-    @PostMapping(value = "api/v1/user/register")
+    @PostMapping(value = "/user/register")
     @ResponseStatus(value = HttpStatus.CREATED)
     public void createUser(@RequestBody @Valid User user)
     {
