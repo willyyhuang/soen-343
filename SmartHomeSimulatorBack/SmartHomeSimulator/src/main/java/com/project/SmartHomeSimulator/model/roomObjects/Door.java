521c2ed1b5cd27dfcf87f29c62a3b25ddc951b70
hodansh <hoda.nourbakhsh@mail.concordia.ca>
2020-11-07 01:56:22 +0330

    door has been set up

--- SmartHomeSimulatorBack/SmartHomeSimulator/src/main/java/com/project/SmartHomeSimulator/model/roomObjects/Door.java
+++ SmartHomeSimulatorBack/SmartHomeSimulator/src/main/java/com/project/SmartHomeSimulator/model/roomObjects/Door.java
@@ -1,12 +1,30 @@
 package com.project.SmartHomeSimulator.model.roomObjects;
 
 public class Door extends RoomObject {
+    private boolean isBlocked;
+    private boolean isOpen;
+
+
 
     public Door(){
     }
     public Door(RoomObject roomObject){
         super();
         setObjectType(roomObject.getObjectType());
+        this.isBlocked = false;
+    }
+
+    public boolean isOpen(){return  isOpen;}
+    public void setOpen(boolean open){
+        isOpen=open;
+    }
+
+    public boolean isBlocked() {
+        return isBlocked;
+    }
+
+    public void setBlocked(boolean blocked) {
+        isBlocked = blocked;
     }
 
     @Override
@@ -15,7 +33,7 @@ public class Door extends RoomObject {
                 "name=" + getName() +
                 ", id=" + getId() +
                 ", roomObjectType=" + getObjectType() +
-                ", status=" + isStatus() +
+                ", isBlocked=" + isBlocked +
                 '}';
     }
 }
