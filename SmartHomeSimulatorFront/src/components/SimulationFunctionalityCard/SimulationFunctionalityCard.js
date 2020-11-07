225bd7a53488936baac7f556ad61ce38a6c5e7da
Wilson Wei Chen Huang <48188884+willyyhuang@users.noreply.github.com>
2020-11-06 19:03:39 -0500

    chore: add clock

--- SmartHomeSimulatorFront/src/components/SimulationFunctionalityCard/SimulationFunctionalityCard.js
+++ SmartHomeSimulatorFront/src/components/SimulationFunctionalityCard/SimulationFunctionalityCard.js
@@ -1,17 +1,38 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react'
 import {
   Card, Row, Col, Typography,
 } from 'antd'
 import moment from 'moment'
+import Clock from 'react-clock'
 import {RoomCard} from '../index'
+import 'react-clock/dist/Clock.css'
 
 const SimulationFunctionalityCard = ({simulationConfig, fetchUserProfiles}) => {
   const {
     simulationRunning, homeLayout, date, time,
   } = simulationConfig
+  const [currentTime, setCurrentTime] = useState(moment(date.concat(time)))
+  const [value, setValue] = useState(new Date(currentTime.format('YYYY-MM-DD HH:mm:ss')))
+
+  useEffect(() => {
+    const interval = setInterval(
+      () => {
+        setCurrentTime(currentTime.add(1, 'seconds'))
+        setValue(new Date(currentTime.format('YYYY-MM-DD HH:mm:ss')))
+      },
+      1000,
+    )
+    return () => {
+      clearInterval(interval)
+    }
+  }, [simulationConfig])
 
   return (
     <Card title='Smart Home Core Functionality'>
+      <div>
+        <p>Current time:</p>
+        <Clock value={value} />
+      </div>
       {simulationRunning && <Row>
         <Typography.Text>
           {`Simulation start time: ${moment(date.concat(time)).format('LLLL')}`}
